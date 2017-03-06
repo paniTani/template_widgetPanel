51f26079534e5051bc3b77e89fdd7c4d99558dfe
Tanya <tanyshka_zp@ukr.net>
2017-03-06 19:45:53 +0200

    added widgetPanel template

---
+++ js/tabs.js
@@ -0,0 +1,27 @@
+$(function()  {
+  
+    $(".tab-panels .tabs li").on("click", function(){
+        var panel = $(this).closest(".tab-panels");
+
+        //figure out which panel ti show
+        panel.find(".tabs li").removeClass("active");
+        $(this).addClass("active");
+        var panelToShow = $(this).attr("rel");
+
+
+        //hide current panel
+        panel.find(".panel.active").slideUp(300, showNextPanel);
+
+        //show next panel
+        function showNextPanel(){
+            $(this).removeClass("active");
+            $("#"+panelToShow).slideDown(300, function(){
+                $(this).addClass("active");
+            });
+        }
+
+        //show new panel
+
+    });
+
+});
